6dcdbaa5d7068679f9efd77fef8f8436f2bb0f96
Criticalories <dennischampion@LIVE.COM>
2022-03-15 19:35:06 +0100

    Uploading full game

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = FindObjectOfType<Transform>().position + offset;
+        //Debug.Log(transform.position.ToString());
+        //Debug.Log(offset);
+        //Debug.Log(FindObjectOfType<GameObject>().name);
+        transform.position = player.position + offset;
+    }
+}
